b6e601c4a2e495de026dc158608e5d1701a030df
ThiaggoSylva <thiaggol.sylva@gmail.com>
2026-03-13 20:20:27 -0300

    Adicionando Exercicios do 13 ao 14

---
+++ Exercicio13/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Digite o valor de A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Digite o valor de B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Digite o valor de C: ");
+int C = Convert.ToInt32(Console.ReadLine());
+
+if (A + B < C)
+{
+    Console.WriteLine("A soma de A + B é menor que C");
+}
+else
+{
+    Console.WriteLine("A soma de A + B NÃO é menor que C");
+}
---
+++ Exercicio14/Program.cs
@@ -0,0 +1,50 @@
+﻿Console.WriteLine("Digite 3 numeros diferentes");
+Console.WriteLine();
+
+Console.Write("Digite o primeiro número: ");
+int A = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Digite o segundo número: ");
+int B = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Digite o terceiro número: ");
+int C = Convert.ToInt32(Console.ReadLine());
+
+//Valida se o usuario nao digitou valores iguais
+if (A == B || A == C || B == C)
+{
+    Console.WriteLine("Erro: Os números devem ser diferentes.");
+    return;
+}
+
+
+// Verifica se A é o maior número
+if (A > B && A > C)
+{
+    if (B > C)
+        // Se B é maior que C, a ordem decrescente é A, B, C
+        Console.WriteLine($"{A}, {B}, {C}");
+    else
+        // Caso contrário, C é maior que B
+        Console.WriteLine($"{A}, {C}, {B}");
+}
+// Verifica se B é o maior número
+else if (B > A && B > C)
+{
+    if (A > C)
+        // Se A é maior que C, a ordem é B, A, C
+        Console.WriteLine($"{B}, {A}, {C}");
+    else
+        // Caso contrário, C é maior que A
+        Console.WriteLine($"{B}, {C}, {A}");
+}
+else
+{
+    // Verificamos quem é maior entre A e B
+    if (A > B)
+        // Se A é maior que B
+        Console.WriteLine($"{C}, {A}, {B}");
+    else
+        // Caso contrário B é maior que A
+        Console.WriteLine($"{C}, {B}, {A}");
+}
