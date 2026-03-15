95a000d87579183c2ad476d38e2bb378fa3f95cf
ThiaggoSylva <thiaggol.sylva@gmail.com>
2026-03-15 14:20:28 -0300

    Adicionado Exercicio 22 a 23

---
+++ Exercicio22/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Digite um número: ");
+        int limite = Convert.ToInt32(Console.ReadLine());
+
+        int a = 0;
+        int b = 1;
+
+        Console.WriteLine("Sequência de Fibonacci:");
+
+        while (a <= limite)
+        {
+            Console.Write(a + " ");
+
+            int proximo = a + b; // soma dos dois números anteriores
+            a = b;               // atualiza o primeiro número
+            b = proximo;        // atualiza o segundo número
+        }
+
+        Console.ReadLine();
---
+++ Exercicio23/Program.cs
@@ -0,0 +1,26 @@
+﻿// Laço que percorre de 1 até 100
+        for (int i = 1; i <= 100; i++)
+        {
+            // Verifica se é múltiplo de 3 e 5
+            if (i % 3 == 0 && i % 5 == 0)
+            {
+                Console.WriteLine("FizzBuzz");
+            }
+            // Verifica se é múltiplo de 3
+            else if (i % 3 == 0)
+            {
+                Console.WriteLine("Fizz");
+            }
+            // Verifica se é múltiplo de 5
+            else if (i % 5 == 0)
+            {
+                Console.WriteLine("Buzz");
+            }
+            // Caso não seja múltiplo
+            else
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        Console.ReadLine();
